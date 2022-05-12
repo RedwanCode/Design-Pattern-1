4be2d8db6430d93b388d0d56fbed3663f682b15c
R.Rana <55597540+ranaswe2@users.noreply.github.com>
2022-05-12 15:27:27 +0600

    Delete StrategyPattern_CP directory

--- StrategyPattern_CP/MulOperation.java
+++
@@ -1,9 +0,0 @@
-package strategy_pattern0;
-
-public class MulOperation implements Strategy{
-
-    @Override
-    public int doOperation(int num1, int num2) {
-        return num1*num2;
-    }
-}
--- StrategyPattern_CP/SubOperation.java
+++
@@ -1,9 +0,0 @@
-package strategy_pattern0;
-
-public class SubOperation implements Strategy{
-
-    @Override
-    public int doOperation(int num1, int num2) {
-        return num2-num1;
-    }
-}
